f8aa23aecdb07dde9089cd8e01dcb57837cc334c
Ishwari Patil <ishwaripatil125@gmail.com>
2026-01-10 15:28:41 +0530

    ass 12

---
+++ ass12.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+print("My Name: Ishwari Patil")
+print("Fun fact: I learn New Tech Skills")
+print("Today's Mood: Happy")
+
+print(type(13))
+print(type(5.314))
+print(type("Pyhton"))
+print(type([1,2,3]))
+print(type((1,2,3)))
+
+# print(5+"10")
+num=10
+num_str=str(num)
+print(type(num_str))
